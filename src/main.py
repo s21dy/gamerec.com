f06d7f01d6e06bf33e437e31dbb44ea3bdf886de
Sandy Yang <yang21@uw.edu>
2025-01-20 19:33:36 +0800

    add debug mode for render

--- src/main.py
+++ src/main.py
@@ -159,4 +159,4 @@ def remove_liked_game():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Default to 5000
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
