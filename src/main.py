ed6423712b9a7d63ac66463b9ab4bd967568560a
Sandy Yang <yang21@uw.edu>
2025-01-21 14:07:05 +0800

    change port

--- src/main.py
+++ src/main.py
@@ -164,4 +164,4 @@ if __name__ == "__main__":
             print(f"Initialization failed: {e}")
             raise
     port = int(os.environ.get("PORT", 8001))  # Default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
